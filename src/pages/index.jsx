349d8f16d6716b8a85d4f38904d5a246cda7f1a9
Аригун Bold <23LP5891@G401-CC0110.local>
2024-02-15 19:01:59 +0800

    okeydokey

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -5,14 +5,28 @@ import {
   Post,
   Footer,
 } from "@/Components/Layout/index";
+import { useState, useEffect } from "react";
 const Home = () => {
+  const [data, setData] = useState([]);
+  const fetchData = async () => {
+    try {
+      const res = await fetch("https://dev.to/api/articles?per_page=1");
+      const data = await res.json();
+      setData(data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  useEffect(() => {
+    fetchData();
+  }, []);
   return (
     <div className="flex flex-col items-center">
       <div className="flex flex-col items-center sm:w-[1440px]">
         <Header />
         <Main />
         <Trending />
-        <Post />
+        {/* <Post /> */}
       </div>
       <Footer />
     </div>
